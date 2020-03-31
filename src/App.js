01f74cb74d6123866dd42938c7a65e8fcc46e443
Trevor Schwab <trevschwab@gmail.com>
2020-03-31 18:33:28 -0500

    Class to functional components w/hooks finished

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SidebarComponent from './sidebar/sidebar';
 import EditorComponent from './editor/editor';
 import './App.css';
 
 const firebase = require('firebase');
 
-class App extends React.Component {
+const App = () => {
 
-  constructor() {
-    super();
-    this.state = {
-      selectedNoteIndex: null,
-      selectedNote: null,
-      notes: null
-    };
-  }
+  const [selectedNoteIndex, setSelectedNoteIndex] = useState(null);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [notes, setNotes] = useState(null);
 
-  
-
-  render() {
-
-    const noteUpdate = (id, noteObj) => {
-      console.log('app id notObj ', id, noteObj)
-      if (id !== null && id !== undefined && id !== "") {
-        firebase
-          .firestore()
-          .collection('notes')
-          .doc(id)
-          .update({
-            title: noteObj.title,
-            body: noteObj.body,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-          });
-      }
-    }
-
-    return(
-      <div className="app-container">
-        <SidebarComponent 
-          selectedNoteIndex={this.state.selectedNoteIndex}
-          notes={this.state.notes}
-          deleteNote={this.deleteNote}
-          selectNote={this.selectNote}
-          newNote={this.newNote}></SidebarComponent>
-        {
-          this.state.selectedNote ?
-          <EditorComponent 
-            selectedNote={this.state.selectedNote}
-            selectedNoteIndex={this.state.selectedNoteIndex}
-            notes={this.state.notes}
-            noteUpdate={noteUpdate}></EditorComponent> 
-          : null
-        }
-      </div>
-    );
-  }
-
-  componentDidMount = () => {
+  useEffect(() => {
     firebase
       .firestore()
       .collection('notes')
@@ -66,27 +21,11 @@ class App extends React.Component {
           data['id'] = _doc.id;
           return data;
         });
-        console.log(notes);
-        this.setState({ notes: notes });
+        setNotes(notes);
       });
-  }
+  }, []);
 
-  selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
-  
-  // const noteUpdate = (id, noteObj) => {
-  //   console.log('app id notObj ', id, noteObj)
-  //   firebase
-  //     .firestore()
-  //     .collection('notes')
-  //     .doc(id)
-  //     .update({
-  //       title: noteObj.title,
-  //       // body: noteObj.body,
-  //       timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  //     });
-  // }
-  
-  newNote = async (title) => {
+  const newNote = async (title) => {
     const note = {
       title: title,
       body: ''
@@ -100,103 +39,69 @@ class App extends React.Component {
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
       });
     const newID = newFromDB.id;
-    await this.setState({ notes: [...this.state.notes, note] });
-    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
-    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+    setNotes([...notes, note]);
+    const newNoteIndex = notes.indexOf(notes.filter(_note => _note.id === newID)[0]);
+    setSelectedNote(notes[newNoteIndex]);
+    setSelectedNoteIndex(newNoteIndex);
   }
-  deleteNote = async (note) => {
-    const noteIndex = this.state.notes.indexOf(note);
-    await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
-    if(this.state.selectedNoteIndex === noteIndex) {
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
-    } else {
-      this.state.notes.length > 1 ?
-      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
-    }
 
-    firebase
-      .firestore()
-      .collection('notes')
-      .doc(note.id)
-      .delete();
+  const selectNote = (note, index) => {
+    setSelectedNoteIndex(index);
+    setSelectedNote(note);
+  };
+
+  const noteUpdate = (id, noteObj) => {
+    if (id !== null && id !== undefined && id !== "") {
+      firebase
+        .firestore()
+        .collection('notes')
+        .doc(id)
+        .update({
+          title: noteObj.title,
+          body: noteObj.body,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+    }
   }
+
+  const deleteNote = async (note) => {
+    const noteIndex = notes.indexOf(note);
+    setNotes(notes.filter(_note => _note !== note))
+    if(selectedNoteIndex === noteIndex) {
+      setSelectedNoteIndex(null);
+      setSelectedNote(null);
+    } else {
+      notes.length > 1 ?
+      selectNote(notes[selectedNoteIndex - 1], selectedNoteIndex - 1) :
+      setSelectedNoteIndex(null);
+      setSelectedNote(null);
+    }
+  firebase
+    .firestore()
+    .collection('notes')
+    .doc(note.id)
+    .delete();
+  }
+
+  return(
+    <div className="app-container">
+      <SidebarComponent 
+        selectedNoteIndex={selectedNoteIndex}
+        notes={notes}
+        deleteNote={deleteNote}
+        selectNote={selectNote}
+        newNote={newNote}></SidebarComponent>
+      {
+        selectedNote ?
+        <EditorComponent 
+          selectedNote={selectedNote}
+          selectedNoteIndex={selectedNoteIndex}
+          notes={notes}
+          noteUpdate={noteUpdate}></EditorComponent> 
+        : null
+      }
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import SidebarComponent from './sidebar/sidebar';
-// import EditorComponent from './editor/editor';
-// import './App.css';
-
-// const firebase = require('firebase');
-
-// const App = () => {
-
-//   const [selectedNote, setSelectedNote] = useState(null)
-//   const [selectedNoteIndex, setSelectedNoteIndex] = useState(null)
-//   const [notes, setNotes] = useState(null)
-
-//   console.log('app state selectedNote, ', selectedNote, 'notes ', notes)
-
-//   useEffect(() => {
-//     firebase
-//       .firestore()
-//       .collection('notes')
-//       .onSnapshot(dbChange => {
-//         const notes = dbChange.docs.map(doc => {
-//           const data = doc.data()
-//           return data
-//         })
-//         // console.log('notes ', notes)
-//         setNotes(notes)
-//       })
-//   }, [])
-
-//   const selectNote = (note, index) => {
-//     // console.log('selectNOte ', note, index)
-//     setSelectedNote(note)
-//     setSelectedNoteIndex(index)
-//   }
-
-//   const newNote = () => {
-//     console.log('newNote')
-//   }
-
-//   const deleteNote = () => {
-//     console.log('deleteNote')
-//   }
-
-//   const noteUpdate = (id, noteObj) => {
-//     setSelectedNote(noteObj)
-//     console.log('id ', id, 'noteObj ', noteObj)
-//   }
-
-//   let render = 
-//     <div className="app-container">
-//       <SidebarComponent
-//         selectedNoteIndex={selectedNoteIndex}
-//         notes={notes}
-//         selectNote={selectNote}
-//         deleteNote={deleteNote}
-//         newNote={newNote}>       
-//       </SidebarComponent>
-//       { selectedNote ?
-//         <EditorComponent
-//           selectedNote={selectedNote}
-//           selectedNoteIndex={selectedNoteIndex}
-//           notes={notes}
-//           noteUpdate={noteUpdate}>
-//         </EditorComponent>
-//         : null
-//       } 
-//     </div>
-
-//   return render;
-// }
-
-// export default App;
--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -16,15 +16,15 @@ const SidebarComponent = props => {
     setTitle(null);
     setAddingNote(!addingNote);
   }
-  
-  const updateTitle = (txt) => {
-    setTitle(txt);
-  }
-  
+
   const newNote = () => {
     props.newNote(title);
     setTitle(null);
     setAddingNote(false);
+  }
+  
+  const updateTitle = (txt) => {
+    setTitle(txt);
   }
   
   const selectNote = (n, i) => props.selectNote(n, i);
@@ -78,97 +78,3 @@ const SidebarComponent = props => {
 }
 
 export default withStyles(styles)(SidebarComponent);
-
-
-
-
-// import React, { useState } from 'react';
-// import { withStyles } from '@material-ui/core/styles';
-// import styles from './styles';
-// import List from '@material-ui/core/List';
-// import { Divider, Button } from '@material-ui/core';
-// import SidebarItemComponent from '../sidebaritem/sidebarItem';
-
-// const SidebarComponent = props => {
-
-//   const [state, setState] = useState({
-//     addingNote: false,
-//     title: '',
-//   })
-
-//   const { classes, notes, selectedNoteIndex } = props
-
-//   const newNoteBtnClick = () => {
-//     setState({addingNote: !state.addingNote})
-//     console.log('newNoteBtnClick')
-//   }
-
-//   const updateTitle = () => {
-//     // console.log('update title');
-//   }
-
-//   const newNote = () => {
-//     console.log('new note')
-//   }
-
-//   const selectNote = (n, i) => {
-//     props.selectNote(n, i)
-//     // console.log('SIDEBAR selectNote ', n, i)
-//   }
-//   const deleteNote = () => {
-//     console.log('deleteNote');
-//   }
-
-//   if(notes) {
-//     let render =
-//       <div className={classes.sidebarContainer}>
-//         <Button
-//           onClick={newNoteBtnClick}
-//           className={classes.newNoteBtn}>{state.addingNote ? 'Cancel' : 'New Note'}</Button>
-//           {
-//             state.addingNote ?
-//               <div>
-//                 <input
-//                   type='text'
-//                   className={classes.newNoteInput}
-//                   placeholder='Enter note title'
-//                   onKeyUp={(e) => updateTitle(e.target.value)}>
-//                 </input>
-//                 <Button
-//                   className={classes.newNoteSubmitBtn}
-//                   onClick={newNote}>Submit Note</Button>
-//               </div> :
-//               null
-//           }
-//           <List>
-//             {notes.map((_note, _index) => {
-//               return(
-//                 <div key={_index}>
-//                   <SidebarItemComponent
-//                     _note={_note}
-//                     _index={_index}
-//                     selectedNoteIndex={selectedNoteIndex}
-//                     selectNote={selectNote}
-//                     deleteNote={deleteNote}>
-//                   </SidebarItemComponent>
-//                   <Divider></Divider>
-//                 </div>
-//               )
-//             })}
-//           </List>
-
-//       </div>
-
-//     return render 
-
-//     } else {
-//       let render=<div>Sorry..</div>
-
-//       return render
-//     }
-  
-  
-// }
-
-
-// export default withStyles(styles)(SidebarComponent);
